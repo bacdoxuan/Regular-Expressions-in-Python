1a1c6f377825bdf1287c7fdf3fe7c271a1366209
Do Xuan Bac <bacdoxuan@gmail.com>
2023-05-17 16:59:56 +0700

    update pattern

--- check_ip.py
+++ check_ip.py
@@ -4,7 +4,7 @@ A regex to check if an input is a correct ip or not
 
 import re
 
-pattern = r"^(((25(0|1|2|3|4|5){1})|(1\d\d){1}|((1|2|3|4|5|6|7|8|9)\d){1}|(\d){1})\.){3}((25(0|1|2|3|4|5){1})|(1\d\d){1}|((1|2|3|4|5|6|7|8|9)\d){1}|(\d){1})\b"
+pattern = r"^(((25(0|1|2|3|4|5){1})|(1\d\d){1}|(2(0|1|2|3|4)\d){1}|((1|2|3|4|5|6|7|8|9)\d){1}|(\d){1})\.){3}(((25(0|1|2|3|4|5){1})|(1\d\d){1}|(2(0|1|2|3|4)\d){1}|((1|2|3|4|5|6|7|8|9)\d){1}|(\d){1}))"
 
 def main():
     try:
@@ -19,7 +19,7 @@ def main():
             if checkip:
                 print("\"{}\" is a valid ip".format(ip))
             else:
-                print("\"{}\"1 is NOT a valid ip".format(ip))
+                print("\"{}\" is NOT a valid ip".format(ip))
     except Exception as e:
         print(e)
 
